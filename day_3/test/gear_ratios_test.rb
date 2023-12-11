ea4fb82294719937d6e7e9d5b2b2f4c332cad880
Vincent Cornière <vincent.corniere@gmail.com>
2023-12-11 21:26:52 +0100

    add new file

--- day_3/test/gear_ratios_test.rb
+++ day_3/test/gear_ratios_test.rb
@@ -3,3 +3,19 @@ require_relative '../app/gear_ratios'
 
 class GearRatiosTest < Minitest::Test
 end
+
+=begin
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+
+soluce: 4361
+=end
